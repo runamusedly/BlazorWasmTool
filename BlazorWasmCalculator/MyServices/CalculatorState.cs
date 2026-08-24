0e379986fdc7118dac1d10f9c7e3373fe36ea4c1
tamtaki <runamusedly@outlook.com>
2026-08-24 12:22:06 +0900

    improvement of button input

--- BlazorWasmCalculator/MyServices/CalculatorState.cs
+++ BlazorWasmCalculator/MyServices/CalculatorState.cs
@@ -2,14 +2,14 @@
 {
     public class CalculatorState
     {
-        public string CurrentInput { get; set; } = "0";
+        public string CurrentInput { get; set; } = "";
         public double PreviousValue { get; set; } = 0;
         public string? Operator { get; set; } = null;
         public double Result { get; set; } = 0;
         public string? ProcessCalc { get; set; } = null;
 
         public bool IsShowingResult { get; set; } = false;
-        public bool ShowLimitWarning { get; set; } = false;
+        public bool IsShowingWarning { get; set; } = false;
         public string WarningMessage { get; set; } = "";
     }
 }
